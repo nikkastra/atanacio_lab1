58e6977fb5f9e99e0f147734bb7a48018f47f825
nikkastra <81076897+nikkastra@users.noreply.github.com>
2021-04-06 01:53:56 +0800

    changing content

--- mysite/bulletjournal/views.py
+++ mysite/bulletjournal/views.py
@@ -2,26 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 
-from .forms import IndexCardForm
+from .forms import *
 
 class HomePageView(View):
     def get(self, request):
     	form = IndexCardForm()
-    	return render(request, 'index.html', {'form': form})
-
+    	return render(request, 'base.html', {'form': form})
 
 def index_card_view(request):
 	if request.method == 'POST':
 		form = IndexCardForm(request.POST)
 		if form.is_valid():
-			return HttpResponse(
-				'Hello, {}! Today is gonna be a great day!'.format(
-					form.cleaned_data['name']
-					)
+			return render(request,'base.html', {
+				'name':form.cleaned_data['name']
+				}
 				)
 	else:
 	    form = IndexCardForm()
-	return render(request, 'index.html', {'form': form})
+	return render(request, 'base.html', {'form': form})
 
 
 def profile(request):
